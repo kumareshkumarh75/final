e01bfd3682ce56ddbd5de4eb3620cdd00857e4bd
kumaresh <kumareshkumareee75@gmail.com>
2022-09-22 23:15:31 +0530

    dsdd

--- src/org/com/Sample.java
+++ src/org/com/Sample.java
@@ -4,13 +4,14 @@ public class Sample {
 
 	private void m1() {
 		System.out.println(10);
-		System.out.println(212121);
-		System.out.println("qwwwq");
+		
 	}
 	private void m2() {
 		System.out.println(12);
 	}
 	private void m3() {
 		System.out.println(22);
+		System.out.println("dddddddddddddddd");
 	}
+	
 }
