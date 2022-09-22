b6bb93df7059c4f5b0379f559c18c68d18396d8b
kumaresh <kumareshkumareee75@gmail.com>
2022-09-22 23:04:01 +0530

    done tom

--- src/org/com/Sample.java
+++ src/org/com/Sample.java
@@ -4,6 +4,8 @@ public class Sample {
 
 	private void m1() {
 		System.out.println(10);
+		System.out.println(212121);
+		System.out.println("qwwwq");
 	}
 	private void m2() {
 		System.out.println(12);
